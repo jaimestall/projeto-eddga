469f0eb16c6a67aa93d39b1b2b88e6e97e6529ed
jaime filho <jaimestall94@gmail.com>
2022-04-21 22:44:52 -0300

    primeiros estudos com components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <PrimeiroComponente />
+  <p>O componente que abriga componentes também pode conter dados. {{ info }}</p>
 </template>
 
 <script>
@@ -7,5 +8,13 @@ import PrimeiroComponente from './components/PrimeiroComponente.vue'
 
 export default {
   name: 'App',
+  components: {
+    PrimeiroComponente,
+  },
+  data () {
+    return {
+      info: '>>Exemplo de dado trazido do data() do componente principal<<',
+    }
+  }
 }
 </script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>Olá, Vue</h1>
+  <div class="component">
+    <h1>Component principal</h1>
+    <p>Nome: {{ name }}</p>
+    <p>Elemento: {{ elemento }}</p>
+  </div>
 </template>
 
 <script>
 export default {
-  
-}
+  name: "PrimeiroComponente",
+  data() {
+    return {
+      name: "Eddga",
+      elemento: "Fogo",
+    };
+  },
+};
 </script>
+<style scoped>
+.component {
+  border: 1px solid black;
+  padding-left: 1em;
+}
+</style>
